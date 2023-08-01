408b0c53907acd33d89dcab53e145b1c18817798
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-01 15:42:33 +0300

    update settings.py

--- local_library/local_library/settings.py
+++ local_library/local_library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Add our new application
+    'catalog.apps.CatalogConfig', #This object was created for us in /catalog/apps.py
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Europe/Sofia'
 
 USE_I18N = True
 
