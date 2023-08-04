485324d8a8afd3bcb5867bdea4c6411d163c0783
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-04 14:49:12 +0300

    add Book model

--- local_library/catalog/models.py
+++ local_library/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -7,4 +8,27 @@ class Genre(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+    
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
 
