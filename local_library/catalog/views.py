45910489f1ba4771f138c46c8e877e07c8402a39
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-08 12:44:25 +0300

    add num_visits

--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -12,11 +12,15 @@ def index(request):
 
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
@@ -31,7 +35,7 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     """Generic class-based detail view for a book."""
     model = Book
-    
+
 
 class AuthorListView(generic.ListView):
     """Generic class-based list view for a list of authors."""
