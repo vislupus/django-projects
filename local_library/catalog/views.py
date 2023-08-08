bdde786caafa7671db0e50b7328f5e6b255c9abc
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-08 12:19:22 +0300

    creating templates

--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
 
     num_books = Book.objects.all().count()
-    
+
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
@@ -19,3 +20,25 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
+    
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
