414c4c51d4324b3ee4b1415f9a2a3c332fee5d09
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-15 11:39:38 +0300

    update diary project

--- my-diary/entries/views.py
+++ my-diary/entries/views.py
@@ -1,9 +1,46 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Entry
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 class EntryListView(ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
 
+
 class EntryDetailView(DetailView):
     model = Entry
+
+
+class EntryCreateView(SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+
+
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry was updated!"
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.id}) # type: ignore
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
