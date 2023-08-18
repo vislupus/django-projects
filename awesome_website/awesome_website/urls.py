bd9b3ab4e076ec00888338eb753426921d6f7d88
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-18 14:34:11 +0300

    add change password form

--- awesome_website/awesome_website/urls.py
+++ awesome_website/awesome_website/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import include, url
+from django.urls import path, include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^", include("users.urls")),
-    url(r"^admin/", admin.site.urls),
+    re_path(r"^", include("users.urls")),
+    re_path(r"^admin/", admin.site.urls),
 ]
--- awesome_website/users/urls.py
+++ awesome_website/users/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.urls import path, include, re_path
 from users.views import dashboard
 
 urlpatterns = [
-    url(r"^dashboard/", dashboard, name="dashboard"),
+    re_path(r"^dashboard/", dashboard, name="dashboard"),
+    re_path(r"^accounts/", include("django.contrib.auth.urls")),
 ]
