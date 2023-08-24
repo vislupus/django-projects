f94dbeaf128c776165685822501bebc3c709b262
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-24 11:35:11 +0300

    update django_test_project

--- django_test_project/blog/views.py
+++ django_test_project/blog/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+        'title': 'Zen of Python'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html')
--- django_test_project/start_django.bat
+++ django_test_project/start_django.bat
@@ -1,5 +1,6 @@
 @echo off
-echo http://127.0.0.1:8000
+
 start chrome http://127.0.0.1:8000
 python manage.py runserver %*
+
 pause
