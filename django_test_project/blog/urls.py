a270c6918e1cc64ad33ed2a81c3e2721029bb852
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-08-24 14:57:27 +0300

    update django_test_project with forms

--- django_test_project/blog/urls.py
+++ django_test_project/blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="posts"),
     path("post/create", views.create_post, name="post-create"), # type: ignore
+    path('post/edit/<int:id>/', views.edit_post, name='post-edit'), # type: ignore
+    path('post/delete/<int:id>/', views.delete_post, name='post-delete'), # type: ignore
     path("about/", views.about, name="about"),
 ]
--- django_test_project/blog/views.py
+++ django_test_project/blog/views.py
@@ -1,30 +1,61 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
-
-
-def create_post(request):
-    if request.method == 'GET':
-        context = {'form': PostForm()}
-        return render(request, 'blog/post_form.html', context)
-    elif request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('posts')
-        else:
-            return render(request, 'blog/post_form.html', {'form': form})
 
 
 def home(request):
     posts = Post.objects.all()
 
-    context = {
-        'posts': posts,
-        'title': 'Zen of Python'
-    }
-    return render(request, 'blog/home.html', context)
+    context = {"posts": posts, "title": "Zen of Python"}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, "blog/about.html")
+
+
+def create_post(request):
+    if request.method == "GET":
+        context = {"form": PostForm()}
+        return render(request, "blog/post_form.html", context)
+
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "The post has been created successfully.")
+            return redirect("posts")
+        else:
+            messages.error(request, "Please correct the following errors:")
+            return render(request, "blog/post_form.html", {"form": form})
+
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "GET":
+        context = {"form": PostForm(instance=post), "id": id}
+        return render(request, "blog/post_form.html", context)
+
+    elif request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "The post has been updated successfully.")
+            return redirect("posts")
+        else:
+            messages.error(request, "Please correct the following errors:")
+            return render(request, "blog/post_form.html", {"form": form})
+
+
+def delete_post(request, id):
+    post = get_object_or_404(Post, pk=id)
+    context = {"post": post}
+
+    if request.method == "GET":
+        return render(request, "blog/post_confirm_delete.html", context)
+    elif request.method == "POST":
+        post.delete()
+        messages.success(request, "The post has been deleted successfully.")
+        return redirect("posts")
