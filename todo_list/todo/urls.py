8552acda5c01713cc3d0b0f1fb2440ef92a51c46
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-09-21 11:09:05 +0300

    add todo

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import home, TaskList
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", home, name="home"),
+    path("tasks/", TaskList.as_view(), name="tasks"),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
 
 
 def home(request):
