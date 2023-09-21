82e8dcbf2ad1dff52c5b8846b9f5135b8e89770d
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-09-21 15:51:46 +0300

    update todo

--- todo_list/users/models.py
+++ todo_list/users/models.py
@@ -18,7 +18,6 @@ class Profile(models.Model):
         # save the profile first
         super().save(*args, **kwargs)
 
-        # resize the image
         img = Image.open(self.avatar.path)
 
         if img.width > 300 or img.height > 300:
@@ -29,15 +28,6 @@ class Profile(models.Model):
                 new_width = max(300, 300)
                 new_height = max(300, img.height * 300 // img.width)
 
-            img = img.resize((new_width, new_height))
-
-        if not img.height == img.width:
-
-            left = 0
-            top = 0
-            right = int((img.width + 300) / 2)
-            bottom = int((img.height + 300) / 2)
-            img = img.crop((left, top, right, bottom))
-
+            img = img.resize((new_width, new_height)).crop((0, 0, 300, 300))
 
         img.save(self.avatar.path)
