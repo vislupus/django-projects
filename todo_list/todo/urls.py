ab8afffe8d968d41ae5d584765ebefa4b0d8b96b
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-09-21 11:30:10 +0300

    add create form

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, TaskList
+from .views import home, TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path("", home, name="home"),
     path("tasks/", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task/create/", TaskCreate.as_view(), name="task-create"),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from .models import Task
+
 
 class TaskList(ListView):
     model = Task
-    context_object_name = 'tasks'
+    context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ["title", "description", "completed"]
+    success_url = reverse_lazy("tasks")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "The task was created successfully.")
+        return super(TaskCreate, self).form_valid(form)
 
 
 def home(request):
